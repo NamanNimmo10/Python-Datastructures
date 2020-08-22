ea41c1d490eb4383ad2c8a7b093c26e0bf3e9a8b
Naman Gera <namangera15@gmail.com>
2020-08-22 19:04:17 +0530

    Update Assignment9.4.py

--- Assignment9.4.py
+++ Assignment9.4.py
@@ -1,19 +1,18 @@
 name = input("Enter file:")
 handle = open(name)
 
-counts = dict() #creating a dictionary
+counts = dict()
 for name in handle:
     name = name.rstrip()
     if not name.startswith('From '):
         continue
     splitIt = name.split()
-    counts[splitIt[1]] = counts.get(splitIt[1],0) + 1
+    counts[splitIt[1]] = counts.get(splitIt[1], 0) + 1
     
         
-bigcount = None
-bigword = None
-for word,count in counts.items():
-    if bigcount is None or count>bigcount:
-        bigword = word
-        bigcount = count
+bigcount, bigword = None, None
+for word, count in counts.items():
+    if bigcount is None or count > bigcount:
+        bigword, bigcount = word, count
+
 print(bigword, bigcount)
